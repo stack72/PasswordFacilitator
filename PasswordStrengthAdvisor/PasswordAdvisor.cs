55260c7374fd9ad6bd98179db43eb5dba15d95b9
Paul Stack <paulstack@hotmail.com>
2011-07-31 01:53:48 +0100

    renaming of the Facility to be Frontend and also addition of the StrengthAdvisor

---
+++ PasswordStrengthAdvisor/PasswordAdvisor.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PasswordStrengthAdvisor
+{
+    public class PasswordAdvisor
+    {
+        public static PasswordScore CheckStrength(string password)
+        {
+            int score = 1;
+
+            if (password.Length < 1)
+                return PasswordScore.Blank;
+            if (password.Length < 4)
+                return PasswordScore.VeryWeak;
+
+            if (password.Length >= 8)
+                score++;
+            if (password.Length >= 12)
+                score++;
+            if (Regex.Match(password, @"/\d+/", RegexOptions.ECMAScript).Success)
+                score++;
+            if (Regex.Match(password, @"/[a-z]/", RegexOptions.ECMAScript).Success &&
+                Regex.Match(password, @"/[A-Z]/", RegexOptions.ECMAScript).Success)
+                score++;
+            if (Regex.Match(password, @"/.[!,@,#,$,%,^,&,*,?,_,~,-,£,(,)]/", RegexOptions.ECMAScript).Success)
+                score++;
+
+            return (PasswordScore)score;
+        }
+    }
+}
